9af6a7e7fffbd62a09308a34f9773f04f5b3bb38
AJ <47323884+AjayKiran1@users.noreply.github.com>
2020-12-12 14:10:16 +0530

    PlayerMovement
    Player Movement
    Joystick Controls + Fixed Buttons
    Touch Field - Camera Look

---
+++ Scripts/Joystick/FixedButtonAssigner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FixedButtonAssigner : MonoBehaviour
+{
+    [SerializeField] FixedButton[] fixedButtons;
+    [SerializeField] FixedButton[] fixedButtonsList = new FixedButton[2];
+    private void Awake()
+    {
+        fixedButtons = FindObjectsOfType<FixedButton>();
+        foreach(FixedButton f in fixedButtons)
+        {
+            if(f.name=="JumpButton")
+            {
+                fixedButtonsList[0] = f;
+            }
+            if (f.name == "CrouchButton")
+            {
+                fixedButtonsList[1] = f;
+            }
+        }
+    }
+    public FixedButton[] GetFixedButtons()
+    {
+        return fixedButtonsList;
+    }
+}
---
+++ Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] FixedJoystick fixedJoystick;
+    [SerializeField] FixedButton jumpButton;
+    [SerializeField] FixedButton crouchButton;
+    [SerializeField] FixedButtonAssigner fba;
+    [SerializeField] ThirdPersonUserControl control;
+    [SerializeField] FixedTouchField touchField;
+    [SerializeField] Camera camera;
+    [SerializeField] float cameraAngle, cameraSpeed=0.2f,rotOffset;
+    [SerializeField] Vector3 cameraOffset;
+    void Start()
+    {
+        fixedJoystick = FindObjectOfType<FixedJoystick>();
+        fba = GetComponent<FixedButtonAssigner>();
+        jumpButton = fba.GetFixedButtons()[0];
+        crouchButton = fba.GetFixedButtons()[1];
+        control = GetComponent<ThirdPersonUserControl>();
+        touchField = FindObjectOfType<FixedTouchField>();
+        camera = FindObjectOfType<Camera>();
+    }
+    void Update()
+    {
+        control.m_Jump = Input.GetKey("space") || jumpButton.Pressed;
+        control.m_Crouch = Input.GetKey("c") || crouchButton.Pressed;
+        control.hInput = Input.GetAxis("Horizontal") + fixedJoystick.Horizontal;
+        control.vInput = Input.GetAxis("Vertical") + fixedJoystick.Vertical;
+
+        cameraAngle += touchField.TouchDist.x * cameraSpeed;
+
+        camera.transform.position = transform.position + Quaternion.AngleAxis(cameraAngle, Vector3.up) * cameraOffset;
+        camera.transform.rotation = Quaternion.LookRotation(transform.position+Vector3.up* rotOffset - 
+            camera.transform.position
+            , Vector3.up);
+    }
+}
