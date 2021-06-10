fa1542dde8dad0ec158eac0f46540d056a79439e
AJ <47323884+AjayKiran1@users.noreply.github.com>
2021-06-10 08:41:47 +0530

    Projectiles
    Projectiles

--- Scripts/Core/Projectiles.cs
+++ Scripts/Core/Projectiles.cs
@@ -5,27 +5,64 @@ using UnityEngine;
 public class Projectiles : MonoBehaviour
 {
     [SerializeField] float speed = 10f, deactivateAfter = 15f, damage = 3f;
-    [SerializeField] string instantiator;
+    [SerializeField] bool deactivateOnHit = true,moveToPosition=true;
+    [SerializeField] string instantiator="default";
+    [SerializeField] Vector3 targetPos;
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (moveToPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos,
+                speed * Time.deltaTime);
+        }
+        if(transform.position==targetPos)
+        {
+            moveToPosition = false;
+        }
+        if (!moveToPosition)
+            transform.Translate(transform.forward * speed/4 * Time.deltaTime);
+
+        if (transform.position.y <= -0.2f)
+        { StartCoroutine(Deactivate(0.1f)); }
         StartCoroutine(Deactivate(deactivateAfter));
     }
     IEnumerator Deactivate(float time)
     {
         yield return new WaitForSeconds(time);
+        moveToPosition = true;
+        yield return new WaitForSeconds(0.1f);
         gameObject.SetActive(false);
     }
     public void SetInstantiator(string instantiator)
     {
-        instantiator = this.instantiator;
+        this.instantiator = instantiator;
+    }
+    public string GetInstantator()
+    {
+        return instantiator;
+    }
+    public void SetAimLocation(Vector3 tPos)
+    {
+        targetPos = new Vector3(tPos.x,transform.position.y, tPos.z);
+        moveToPosition = true;
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(instantiator!=other.GetComponent<Health>().GetPlayer())
+        bool isPlayer = other.GetComponent<Fighter>();
+        bool isEnemy = !other.GetComponent<Fighter>() && other.GetComponent<xHealth>();
+        if (isPlayer || isEnemy)
         {
-            other.GetComponent<Health>().TakeDamage(damage);
-            StartCoroutine(Deactivate(0.1f));
+            if (isPlayer && instantiator != other.GetComponent<xFighter>().GetPlayerID())
+            {
+                other.GetComponent<xHealth>().TakeDamage(damage, other.gameObject);
+            }
+            else if(isEnemy)
+            {
+                other.GetComponent<xHealth>().TakeDamage(damage, other.gameObject);
+            }
+            if (deactivateOnHit)
+            { StartCoroutine(Deactivate(0.1f)); }
         }
     }
+
 }
